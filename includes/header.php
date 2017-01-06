5f295430839ea2ec562bfda170d48e19d5eee5af
Bret Wagner <bretwagner@bwagner-webdev.com>
2017-01-06 16:36:17 -0600

    tweaked placement of the login/profile link
    Moved login/profile link to the top right corner

--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,7 @@ if (isset($page_title) && $page_title != '') {
     <a href="<?php echo DIR;?>">Home</a>
     <a href="<?php echo DIR . '/all-games.php/'; ?>">View Games</a>
 
-    <?php if(isset($_SESSION['authorized'])) { ?><a href="<?php echo DIR . "/user-profile.php"; ?>">Profile</a><?php } else { ?><a href="<?php echo DIRADMIN;?>">Login</a><?php } ?>
+    <?php if(isset($_SESSION['authorized'])) { ?><a href="<?php echo DIR . "/user-profile.php"; ?>" style="position:absolute; top:10px; right:10px;" >Profile</a><?php } else { ?><a href="<?php echo DIRADMIN;?>" style="position:absolute; top:10px; right:10px;" >Login</a><?php } ?>
         </div>
     </div>
     <!-- END NAV -->
