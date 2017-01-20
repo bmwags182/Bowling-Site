caf0caa258d44de6df63eee04383f66be74babfe
bmwags182 <bretwagner@bwagner-webdev.com>
2017-01-20 00:29:48 -0600

    Unfinished edits
    but I need sleep...
    
    seriously, I haven't slept in three days.
    
    still updating the classes file for better structure and extendability
    later

--- includes/classes.php
+++ includes/classes.php
@@ -5,28 +5,42 @@
 +--------------------------------------------------------+
 | Author: Bret Wagner  Email: bretwagner@bwagner-webdev.com
 +--------------------------------------------------------+*/
+require('../includes/config.php');
 
 class User {
     private $username;
     private $password;
+    private $email;
     private $id;
+    private $data[];
 
-    public function __construct($id, $username, $password) {
+    public function __construct($id, $username, $email, $password, $data) {
         $this->id = $id;
         $this->username = $username;
+        $this->email = $email;
         $this->password = $password;
+        $this->data = [
+                        'admin'         => '0',
+                        'first_name'    => '',
+                        'last_name'     => '',
+                        'birthday'      => '',
+                        'join_date'     => date(m, d, Y),
+                        'avatar'        => '',
+                        'location'      => '',
+                        'quote'         => '',
+                        'about_me'      => '']
     }
 
     public function login($username, $password) {
         $mysqli = db_connect();
         //strip all tags from varible
-        $user = strip_tags(mysqli_real_escape_string($mysqli,$user));
-        $pass = strip_tags(mysqli_real_escape_string($mysqli,$pass));
+        $username = strip_tags(mysqli_real_escape_string($mysqli,$username));
+        $password = strip_tags(mysqli_real_escape_string($mysqli,$password));
 
-        $pass = md5($pass);
+        $password = md5($password);
 
         // check if the user id and password combination exist in database
-        $sql = "SELECT * FROM members WHERE username = '$user' AND password = '$pass'";
+        $sql = "SELECT * FROM members WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($mysqli, $sql) or die('Query failed. ' . mysqli_error($mysqli));
         $result_array = mysqli_fetch_array($result);
 
@@ -52,35 +66,9 @@ class User {
         header('Location: '.DIRADMIN.'login.php');
         exit();
     }
-}
 
-class UserData() {
-    private $user_id;
-    private $data_id;
-    private $admin;
-    private $first_name;
-    private $last_name;
-    private $email;
-    private $birthday;
-    private $join_date;
-    private $avatar;
-    private $location;
-    private $quote;
-    private $about_me;
+    public function create_user($username, $email, $password) {
 
-    public function __contruct($user_id, $data_id, $admin, $first_name, $last_name, $email, $birthday, $join_date, $avatar, $location, $quote, $about_me) {
-        
-        $this->user_id = $user_id;
-        $this->data_id = $data_id;
-        $this->admin = $admin;
-        $this->first_name = $first_name;
-        $this->lasst_name = $last_name;
-        $this->email = $email;
-        $this->birthday = $birthday;
-        $this->join_date = $join_date;
-        $this->avatar = $avatar;
-        $this->location = $location;
-        $this->quote = $quote;
-        $this->about_me = $about_me;
     }
 }
+
