e09bf8f69c5d3e4cf6c17fad74be00f53bae026e
bmwags182 <bretwagner@bwagner-webdev.com>
2017-03-05 10:08:40 -0600

    small typo
    fixed a small typo on the login page

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,7 @@
 | Author: Bret Wagner  Email: bretwagner@bwagner-webdev.com
 +--------------------------------------------------------+*/
 
- require('../includes/config.php');
+require('../includes/config.php');
 if(logged_in()) {header('Location: '.DIRADMIN);}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
